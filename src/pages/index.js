609cddf0616d9873fed659cd13f7fc35f820f68a
dmnovikov <dj.novikov@gmail.com>
2023-07-20 20:02:53 +0500

    11

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,11 +15,13 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
+          /*
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
             Docusaurus Tutorial - 15min ⏱️
           </Link>
+          */
         </div>
       </div>
     </header>
